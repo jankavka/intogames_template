2dea3cc104d8e6ec25dd3e6e42ff3542b42c2b9f
jankavka <jankavka@seznam.cz>
2025-10-11 17:33:01 +0200

    added some assets, updated css, responsivity for mobile

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+document.addEventListener("DOMContentLoaded", function () {
   //changes order of componenets on mobile device
 
   let reduction = function () {
+    //elements from contact section
     let spec = document.getElementById("pic-element");
     let parent = spec.parentNode;
-    if (window.innerWidth <= 576 && spec.parentNode) {
-      parent.removeChild(spec);
 
+    //elements from gamedevs section
+    let gameLogo = document.getElementById("gamedevs_logo");
+    let gameParent = document.getElementById("gamedevs_logo_parent")
+    let mobDevGamePar = document.getElementById("content-box")
+    
+    if (window.innerWidth <= 576 ) {
+
+      //removing element from gamedev section and appending it to the end of section
+      gameParent.removeChild(gameLogo)
+      mobDevGamePar.appendChild(gameLogo)
+      
+      //removing pic from conact section and appending it as first element
+      parent.removeChild(spec);
       parent.appendChild(spec);
-      console.log("triggered");
-    } else if (window.innerWidth > 576 && spec.parentNode) {
+  
+    } else if (window.innerWidth > 576) {
+      //resizing to big screeen
       parent.removeChild(spec);
       parent.prepend(spec);
-      console.log("Done");
+
+      //inserts gamedevs logo to first place in gameParent
+      gameParent.prepend(gameLogo)
     }
   };
 
